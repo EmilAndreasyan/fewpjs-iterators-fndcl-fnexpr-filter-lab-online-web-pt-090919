0d9b7028432e68beda7269852ab38e1188eaf2df
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-12 20:28:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Code your solution here
 // const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-function findMatching (drivers, testString) {
-  return drivers.filter(driver => {
-    driver.toLowerCase() === testString.toLowerCase()
-  })
+// function findMatching (drivers, testString) {
+//   return drivers.filter(driver => {
+//     driver.toLowerCase() === testString.toLowerCase()
+//   })
+// }
+
+function findMatching(source, sought) {
+  return source.filter( possibleMatch =>
+    possibleMatch.toLowerCase() === sought.toLowerCase()
+  )
 }
 
 
