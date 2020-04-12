63d8604518fd35f1d525a0dbfaaf64d4bd5fe2b9
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-12 20:18:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,19 +7,11 @@ function findMatching (drivers, name) {
   })
 }
 
-// function fuzzyMatch (drivers, name) {
-//   return drivers.filter(driver => {
-//     driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
-//   })
-// }
-
-
-function fuzzyMatch(source, testString) {
-  return source.filter( possibleMatch =>
-    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-  )
+function fuzzyMatch (drivers, name) {
+  return drivers.filter(driver => {
+    driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
+  })
 }
-
 
 function matchName(drivers, givenName) {
   return drivers.filter( driver => driver.name === givenName)
